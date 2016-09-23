17b57a1422387f0217ec60b3359b7c574cc882b5
sareduhh <sarahe.kaminski@gmail.com>
2016-09-23 11:36:23 -0400

    first commit

---
+++ ReserveAccount.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountProject
+{
+    class ReserveAccount : AccountBase
+    {
+        //fields (2)
+
+        //properties (2)
+
+        //constructor (1)
+
+        //methods
+    }
+}
---
+++ SavingsAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountProject
+{
+    class SavingsAccount : AccountBase
+    {
+        //fields (2)
+
+        //properties (2)
+
+        //constructor (1)
+
+        //methods
+
+    }
+}
